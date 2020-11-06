6c351286253ceaee8f143564de6d75640fc0c1f1
Youga810 <pron.youg810@gmail.com>
2020-11-06 10:53:30 +0900

    Update user_login.php

--- kadai3/kadai3_3/user_login.php
+++ kadai3/kadai3_3/user_login.php
@@ -1,8 +1,8 @@
 <?php
 
-  $dsn = 'mysql:dbname=co_19_265_99sv_coco_com;host=localhost';
+  $dsn = 'mysql:dbname=****************;host=localhost';
   $user = 'co-19-265.99sv-c';
-  $password = 'N5dDfJih';
+  $password = '********';
   #DB接続
   try{
     $dbh = new PDO($dsn, $user, $password);
