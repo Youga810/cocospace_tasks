9dc4181710a3439ff1b7cc3b0ab58bcd77df885c
Youga810 <pron.youg810@gmail.com>
2020-11-06 10:54:05 +0900

    Update user_register.php

--- kadai3/kadai3_3/user_register.php
+++ kadai3/kadai3_3/user_register.php
@@ -34,9 +34,9 @@
 登録済みID パスワード(デバッグ用)
 <br>
 <?php
-$dsn = 'mysql:dbname=co_19_265_99sv_coco_com;host=localhost';
+$dsn = 'mysql:dbname=****************;host=localhost';
 $user = 'co-19-265.99sv-c';
-$password = 'N5dDfJih';
+$password = '********';
 #DB接続
 try{
   $dbh = new PDO($dsn, $user, $password);
